73a211d09318144e9dd0caec52dcadd0c7e36ae7
jthur <joaoarthurcontato14@gmail.com>
2026-09-05 01:32:43 -0300

    Update and rename config.example.js to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://rvmldhobvdauckhisncp.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_uhuUXR0ZRNDuRKIoq-SJaw_nuLEbcMt",
+  SITE_URL: "https://joaoarthurcontato14-design.github.io/joao-arthurs-mind/"
+};
